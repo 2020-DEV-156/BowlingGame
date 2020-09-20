17620289f2781103fdbec1d7c2b4e52766894aa0
2020-DEV-156 <2020-DEV-156@users.noreply.github.com>
2020-09-21 01:08:28 +0200

    UI error handling and cleanup

--- BowlingGame/BowlingGameTests/BowlingGameTests.swift
+++ BowlingGame/BowlingGameTests/BowlingGameTests.swift
@@ -20,96 +20,11 @@ class BowlingGameTests: XCTestCase {
         self.sut = nil
     }
 
-    func test_shouldProvideValidScore() {
-        //Arrange
-
-        //Act
-        self.sut.normalRound(1, 5)
-        let finalScore = self.sut.finalScore
-
-        //Assert
-        XCTAssertTrue(finalScore >= 0, "Game should have valid score.")
-    }
-
-    func test_whenPlayerDidNotScoreInAllRollsOfGame_shouldReturnScore0() {
-        //Arrange
-        self.makeNormalRounds(10, 0, 0)
-
-        //Act
-        let finalScore = self.sut.finalScore
-
-        //Assert
-        XCTAssertEqual(finalScore, 0, "Score of the game should have been 0.")
-    }
-
-    func test_whenNormalGame_shouldReturnValidScore() {
-        //Arrange
-        self.makeNormalRounds(10, 6, 2)
-
-        //Act
-        let finalScore = self.sut.finalScore
-
-        //Assert
-        XCTAssertEqual(finalScore, 80, "Score of the game should have been matched with expected score.")
-    }
-
-    func test_whenAtleastOneSpare_shouldReturnValidScore() {
-        //Arrange
-        self.sut.normalRound(6, 2)
-        self.sut.normalRound(6, 2)
-        self.sut.spareRound(1, 9)
-        self.makeNormalRounds(7, 6, 2)
-
-        //Act
-        let finalScore = self.sut.finalScore
-
-        //Assert
-        XCTAssertEqual(finalScore, 88, "Score of the game should have been matched with expected score.")
-    }
-
-    func test_whenAllSpares_shouldReturnValidScore() {
-        //Arrange
-        self.makeSpareRounds(10, 5, 5)
-        self.sut.bonusRound(5, nil)
-
-        //Act
-        let finalScore = self.sut.finalScore
-
-        //Assert
-        XCTAssertEqual(finalScore, 150, "Score of the game should have been matched with expected score.")
-    }
-
-    func test_whenAtleastOneStrike_shouldReturnValidScore() {
-        //Arrange
-        self.sut.normalRound(2, 2)
-        self.sut.normalRound(5, 3)
-        self.sut.strikeRound()
-        self.makeNormalRounds(7, 1, 2)
-
-        //Act
-        let finalScore = self.sut.finalScore
-
-        //Assert
-        XCTAssertEqual(finalScore, 46, "Score of the game should have been matched with expected score.")
-    }
-
-    func test_whenAllStrikes_shouldReturnValidScore() {
-        //Arrange
-        self.makeStrikeRounds(10)
-        self.sut.bonusRound(10, 10)
-
-        //Act
-        let finalScore = self.sut.finalScore
-
-        //Assert
-        XCTAssertEqual(finalScore, 300, "Score of the game should have been matched with expected score.")
-    }
-
     func test_whenGivenValidSequenceOfRolls_ShouldReturnValidResult() {
         //Arrange
 
         //Act
-        let finalScore = self.sut.getGamesFinalScore(rolls: [4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5])
+        let finalScore = try! self.sut.getGamesFinalScore(rolls: [4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5,4,5])
 
         //Assert
         XCTAssertEqual(finalScore, 90, "Score should have matched")
@@ -119,7 +34,7 @@ class BowlingGameTests: XCTestCase {
         //Arrange
 
         //Act
-        let finalScore = self.sut.getGamesFinalScore(rolls: [10,10,10,10,10,10,10,10,10,10,10,10])
+        let finalScore = try! self.sut.getGamesFinalScore(rolls: [10,10,10,10,10,10,10,10,10,10,10,10])
 
         //Assert
         XCTAssertEqual(finalScore, 300, "Score should have matched")
@@ -129,34 +44,10 @@ class BowlingGameTests: XCTestCase {
         //Arrange
 
         //Act
-        let finalScore = self.sut.getGamesFinalScore(rolls: [5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5])
+        let finalScore = try! self.sut.getGamesFinalScore(rolls: [5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5])
 
         //Assert
         XCTAssertEqual(finalScore, 150, "Score should have matched")
-    }
-
-}
-
-//MARK:- Stub helpers
-
-private extension BowlingGameTests {
-
-    func makeNormalRounds(_ count: Int,_ firstRoll: Int,_ secondRoll: Int) {
-        for _ in 0..<count {
-            self.sut.normalRound(firstRoll, secondRoll)
-        }
-    }
-
-    func makeSpareRounds(_ count: Int,_ firstRoll: Int,_ secondRoll: Int) {
-        for _ in 0..<count {
-            self.sut.spareRound(firstRoll, secondRoll)
-        }
-    }
-
-    func makeStrikeRounds(_ count: Int) {
-        for _ in 0..<count {
-            self.sut.strikeRound()
-        }
     }
 
 }
